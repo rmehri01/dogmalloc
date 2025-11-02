549f39cc9ff5151e0c1c359286e7c644d5fc058c
Ryan Mehri <rmehri01@pm.me>
2025-11-02 17:09:31 -0500

    feat: initial template

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zig = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      utils,
+      ...
+    }@inputs:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            inputs.zig.packages.${system}.master
+          ];
+        };
+      }
+    );
+}
